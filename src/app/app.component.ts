cdf58523c613e56d112604f1904105abb5fe489c
apocalupses <dmirtykrivobokov@gmail.com>
2019-12-05 03:01:48 +0600

    master

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ag1';
+  
+  title = 'Dynamic title';
+  number = 42;;
+  arr = [1,2,3,4];
+  obj = {a:1, b:2};
+
+  inputValue = '';
+
+  img = 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/320px-React-icon.svg.png';
+
+  constructor() {
+    setTimeout(()=>{
+      console.log(this.number++);
+      this.img = 'https://justup.com.ua/wp-content/themes/justup/img/logos/12.png';
+    }, 5000)
+  }
+  onInput(event?: any){
+    console.log(event)
+    this.inputValue  = event.target.value;
+  }
+  onClick(){
+    this.inputValue = "Click"
+  }
 }
